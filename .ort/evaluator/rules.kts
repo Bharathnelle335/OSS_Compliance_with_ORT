6c675e448944d82c9703fed944f877cce55f3f57
Bharath <bharathnelle335@gmail.com>
2025-05-05 13:40:27 +0530

    Initial push: Full Scan_All steps_38

--- .ort/evaluator/rules.kts
+++ .ort/evaluator/rules.kts
@@ -1,15 +1,17 @@
 // .ort/config/evaluation/rules.kts
 
 import org.ossreviewtoolkit.evaluator.EvaluatorRun
-import org.ossreviewtoolkit.evaluator.Severity
+import org.ossreviewtoolkit.model.AdvisorIssue
+import org.ossreviewtoolkit.model.EvaluatorIssue
+import org.ossreviewtoolkit.model.Severity
 import org.ossreviewtoolkit.model.Repository
 
 rules = evaluatorRun {
     rule("NO_VULNERABILITIES") {
         severity = Severity.HINT
         condition {
-            it.advisorIssues.isEmpty()
+            it.advisorIssues.none { issue -> issue.severity >= Severity.MEDIUM }
         }
-        hint("No known vulnerabilities.")
+        hint("No medium or higher severity vulnerabilities found.")
     }
 }
